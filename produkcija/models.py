9c7994ccd93e9104fe92db850b81e79b85834426
Rizque <ritvarsvjakse@gmail.com>
2023-03-28 00:22:35 +0300

    #

--- produkcija/models.py
+++ produkcija/models.py
@@ -19,7 +19,8 @@ class Product(models.Model):
     )
     cenas_mervieniba = models.CharField(max_length=100, choices=mervienibu_veidi)
     p_datums = models.DateTimeField(default=timezone.now)
-    id = models.UUIDField(default=uuid.uuid4)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def __str__(self):
         return self.p_nosaukums
--- saimniecibas/models.py
+++ saimniecibas/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 import uuid
 
 class Profile(models.Model):
-    id = models.UUIDField(default=uuid.uuid4)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     username = models.CharField(max_length=200, blank=True, null=True)
     s_nosaukums = models.CharField(max_length=200)
