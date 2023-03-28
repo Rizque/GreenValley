c14b19b918e1de3e323d8c506d9d62af1d8fffd1
Rizque <ritvarsvjakse@gmail.com>
2023-03-29 01:57:57 +0300

    postgresql

--- green_valley/settings.py
+++ green_valley/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'green_valley.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'rizqueRV777',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
