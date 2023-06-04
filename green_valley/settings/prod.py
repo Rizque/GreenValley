73dacc5aca8362406f6fc5ac4313d05c9d4db066
Rizque <ritvarsvjakse@gmail.com>
2023-06-04 20:32:34 +0300

    #

--- green_valley/settings/prod.py
+++ green_valley/settings/prod.py
@@ -31,7 +31,6 @@ AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
